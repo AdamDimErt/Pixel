de091c8f6f3a541ba03cd8c7e0dd3c5007a25c48
Alexander Li <alexander.li@freedompay.money>
2024-02-18 21:08:33 +0600

    footer fix

--- resources/views/brand/footer.blade.php
+++ resources/views/brand/footer.blade.php
@@ -1,3 +1,3 @@
 <p class="h12 n-m font-thin v-center">
-    <small class="v-top opacity">Разработано компанией <b><a href="https://msbtrust.kz/">MSBtrust</a></b></small>
+    <small class="v-top opacity">Разработано компанией <b><a href="https://msbtrust.kz/">MSB Trust</a></b></small>
 </p>
