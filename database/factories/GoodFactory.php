31f1590191f0e5998c03b02a604d63b39069de5f
Alexander Li <alexander.li@freedompay.money>
2024-04-28 02:18:40 +0500

    pdf logic

--- database/factories/GoodFactory.php
+++ database/factories/GoodFactory.php
@@ -22,6 +22,7 @@ class GoodFactory extends Factory
             'description_en' => fake()->unique()->safeEmail(),
             'cost' => fake()->numberBetween(10000, 50000),
             'discount_cost' => rand(0, 1) ? fake()->numberBetween(10000, 50000) : null,
+            'additional_cost' => 0,
             'related_goods' => json_encode(Good::query()->inRandomOrder()->pluck('id')),
             'additionals' => json_encode(Good::query()->inRandomOrder()->pluck('id')),
             'good_type_id' => GoodType::query()->inRandomOrder()->first(),
