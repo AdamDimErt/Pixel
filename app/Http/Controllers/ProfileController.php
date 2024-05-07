c5ab2ec6426cff90691e56d506c5dc2a7e228b1d
Alexander Li <alexander.li@freedompay.money>
2024-05-07 23:03:44 +0500

    profile logic debugging

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -21,8 +21,6 @@ class ProfileController extends Controller
     {
         $client = Client::query()->find(Auth::guard('clients')->id());
 
-        dd(Auth::guard('clients')->id());
-
         if (is_null($client)) {
             return redirect(route('logout'));
         }
