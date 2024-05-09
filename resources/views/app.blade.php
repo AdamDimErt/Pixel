b48ecc6e94e092110596a068757bdc2e12375d1f
Alexander Li <alexander.li@freedompay.money>
2024-05-09 17:40:21 +0500

    ordering logic fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="format-detection" content="telephone=no">
     <title>PixelRental</title>
     @stack('styles')
     <link rel="stylesheet" href="{{asset('css/materialize.css')}}">
